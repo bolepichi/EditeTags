e353422c4c60f09fea71656136876e4838c5822c
Leslie Han <lesliehan9210@gmail.com>
2014-10-29 16:46:17 +0800

    Initial Commit

---
+++ EditeTags/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EditeTags
+//
+//  Created by Taagoo'iMac on 14/10/29.
+//  Copyright (c) 2014年 Taagoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EditeTags/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EditeTags
+//
+//  Created by Taagoo'iMac on 14/10/29.
+//  Copyright (c) 2014年 Taagoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
